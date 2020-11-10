297f50693df5bb5a5769b5fab9c4a7da680f8e5a
Ederson Bueno <bueno.ederson@gmail.com>
2020-11-10 20:28:25 -0300

    Alteração para banco santander

--- src/Conta.kt
+++ src/Conta.kt
@@ -1,4 +1,4 @@
-open class Conta (var saldo: Double, val cliente: Cliente) {
+open class Conta (var saldo: Double, cliente: Cliente) {
 
     fun depositarDinheiro(deposito: Double) : Double{
          saldo += deposito
@@ -6,8 +6,14 @@ open class Conta (var saldo: Double, val cliente: Cliente) {
     }
 
     open fun sacarDinheiro(sacar: Double) : Double {
-        saldo -= sacar
-         return saldo
+        if(sacar<= saldo) {
+            saldo -= sacar
+            return saldo
+        }
+        else
+            println("Saldo insuficiente")
+
+        return -1.0
     }
 
     open fun consultarSaldo() : Double {
